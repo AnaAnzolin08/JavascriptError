2b0c52c0336bbf9f6de63e1e2081d5561291c00d
Ana_Anzo <157380696+AnaAnzolin08@users.noreply.github.com>
2024-02-08 17:29:02 -0300

    Add files via upload
    Podemos definir Escopo também como um conjunto de regras para encontrar variáveis ou a área em que temos 
    acesso válido a funções ou blocos.
    Temos dois tipos de escopo, 
    Escopo Local  ou a 'acessibilidade' de variáveis, funções e objetos em diferentes partes do códigi
    Escopo Global caixa que envolve todo o nosso código
    Escopo mais restrito, pois qualquer variável

---
+++ Aula4_Escopos.js
@@ -0,0 +1,16 @@
+  const primeiraVariavelDoCodigo = 'texto inicial';
+
+  
+  function printToConsole() {
+   const primeiraVariavelDoCodigo = 'segundo texto';
+
+    function secondFunction() {
+      const primeiraVariavelDoCodigo = 'terceiro texto';
+      console.log(primeiraVariavelDoCodigo);
+    }
+
+    console.log(primeiraVariavelDoCodigo);
+  secondFunction();
+}
+
+printToConsole();
